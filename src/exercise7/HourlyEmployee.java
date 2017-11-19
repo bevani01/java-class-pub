51b79bfbea22cb60a4d461c1fac252c4588c69ff
Nick Bevans <bevani01@luther.edu>
2017-11-18 17:20:04 -0800

    exercise7

---
+++ src/exercise7/HourlyEmployee.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise7;
+
+/**
+ *
+ * @author bevani01
+ */
+public class HourlyEmployee  extends Employee {
+    private double wageRate;
+    private double hours;
+    
+    public HourlyEmployee(String name, String ssn, double wageRate, double hours) {
+        super(name, ssn);
+        this.wageRate = wageRate;
+        this.hours = hours;
+    }
+
+    public double getWageRate() {
+        return wageRate;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setWageRate(double wageRate) {
+        this.wageRate = wageRate;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+    
+    @Override
+    public double getNetPay() {
+        return netPay;
+    }
+    
+    /**
+     *
+     */
+    @Override
+    public void printWeeklyCheck() {
+        double weekly_check = 0;
+        weekly_check = hours * wageRate / 52;
+        System.out.print(weekly_check);
+}
+}
