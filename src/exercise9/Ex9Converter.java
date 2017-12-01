e52e6a815f84232ff3df4eb55c55a3b47ce8c3dd
Nick Bevans <bevani01@luther.edu>
2017-11-30 19:40:52 -0800

    exercise9

--- src/exercise9/Ex9Converter.java
+++ src/exercise9/Ex9Converter.java
@@ -2,30 +2,29 @@ package exercise9;
 
 /**
  * Class Ex9Converter
- * @author yasiro01
+ * @author bevani01
  */
 public class Ex9Converter {
-  public Double c2f(Double valueFrom) {
-    throw new UnsupportedOperationException();
+public Double c2f(Double valueFrom) {
+return ((valueFrom * 9/5) + 32);
   }
 
-  public Double c2k(Double valueFrom) {
-    throw new UnsupportedOperationException();
+public Double c2k(Double valueFrom) {
+    return (valueFrom + 273.15);
   }
 
-  public Double f2c(Double valueFrom) {
-    throw new UnsupportedOperationException();
+public Double f2c(Double valueFrom) {
+    return ((valueFrom - 32) * 5/9);
   }
 
-  public Double f2k(Double valueFrom) {
-    throw new UnsupportedOperationException();
+ public Double f2k(Double valueFrom) {
+    return ((valueFrom - 32) * 5/9 + 273.15);
   }
 
-  public Double k2c(Double valueFrom) {
-    throw new UnsupportedOperationException();
+ public Double k2c(Double valueFrom) {
+    return (valueFrom - 273.15);
   }
 
-  public Double k2f(Double valueFrom) {
-    throw new UnsupportedOperationException();
+ public Double k2f(Double valueFrom) {
+    return ((valueFrom - 273.15) * 9/5 + 32);}   
   }
-}
